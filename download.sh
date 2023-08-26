bbd227dcdd6d3de82db70da43371baaf2667f632
AbdullahiAliH <143292727+AbdullahiAliH@users.noreply.github.com>
2023-08-26 23:29:08 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "https://download2.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoibDNjZm5qYXBqazVzeXgzbXI1MnpvOTRyIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQyLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2OTMxNjY0Mzl9fX1dfQ__&Signature=ittcs%7Ej9WX6MoVJITfhQjdNdYkkNc1fflR2ldbGY3BhPyl41ESfjPWEFk1ujDVIv3%7E6Eh2xE6qSxOP6PnXEX5CxOgc4I1WwUnBtGrCl0cnucQnBBacmRE0Mv%7EM768bmnQ%7EhN5uhIZ%7EvFYmh5MtfTGf0KDif1baKv2QuS%7Ey-N0jkBKFFcpIeyhJCLXgsVvmN7Mr-u4hRNjRwSTsodqfLdyTw-Z-YeHuqpEQKWX3dOgYqpNXwBK3SMeovRj2QSarcidVCey8o20GFKaUJ9mDBYSnw8sJJHpLxtmr-svVwCyqU2C1Q7otV4hOpVd6b9Pj0FD9oUFMATa2h76rpOiodxDg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=316137074341085
+" PRESIGNED_URL
 echo ""
 ALL_MODELS="7b,13b,34b,7b-Python,13b-Python,34b-Python,7b-Instruct,13b-Instruct,34b-Instruct"
 read -p "Enter the list of models to download without spaces ($ALL_MODELS), or press Enter for all: " MODEL_SIZE
